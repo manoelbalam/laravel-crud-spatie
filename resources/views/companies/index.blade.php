55116c96b3ec8a7fc0879f646d76d4b0878799e4
isaac balam <manoel.balam@gmail.com>
2025-08-05 15:24:15 -0500

    adding raw query on companies

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -4,6 +4,7 @@
 <div class="row">
     <div class="col-lg-12 margin-tb d-flex justify-content-between align-items-center mb-3">
         <h2>Companies</h2>
+        SELECT id FROM companies WHERE employees > 1000;
         @can('company-create')
             <a class="btn btn-success btn-sm" href="{{ route('companies.create') }}">
                 <i class="fa fa-plus btn-sm"></i>
